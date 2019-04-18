c1a93cc04743a1338db01a11597be7584bde2a46
Benoit Moussaud <bmoussaud@xebialabs.com>
2019-04-18 10:50:03 +0200

    red

--- services/frontend/check.sh
+++ services/frontend/check.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 curl http://localhost:8001/api/v1/proxy/namespaces/staging/services/frontend:80/
+curl http://localhost:8001/api/v1/proxy/namespaces/xl-demo-staging/services/frontend:80/
